f3b15278e74f69901bb9f1066c32b3cfc2cc8b1b
onyxjade <148755965+onyxjade@users.noreply.github.com>
2023-10-24 17:26:27 +0000

    done adding output

--- task1.java
+++ task1.java
@@ -1,1 +1,11 @@
-
+/**
+ * The task1 class implements an application that
+ * simply prints "Kamusta Mundo!" to standard output.
+ */
+class task1 {
+    
+    
+    public static void main(String[] args) {
+    System.out.println("Kamusta Mundo!");
+    }
+}
